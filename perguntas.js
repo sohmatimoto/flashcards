ff743a296c83990be394baa7e6fb8fa892255a39
sohmatimoto <132068329+sohmatimoto@users.noreply.github.com>
2024-10-27 19:20:11 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que e JavaScript?',
+    'O JavaScript  é uma linguagem e programação.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma Função',
+    'Uma função é um bloco codigo que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua Inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em inglês é HI (RAI).'
+)
